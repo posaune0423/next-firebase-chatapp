787579c295d40acbd84d017591256088159180a4
Asuma Yamada <asuma@linear-jp.biz>
2021-04-29 05:56:22 +0900

    fix: add _app.js

---
+++ src/pages/_app.js
@@ -0,0 +1,16 @@
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import '../styles/global.css'
+
+const App = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <link rel="shortcut icon" href="/favicon.png" key="shortcutIcon" />
+      {/* <link rel="manifest" href="/manifest.json" /> */}
+    </Head>
+      <Component {...pageProps} />
+  </>
+)
+
+export default App
