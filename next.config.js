2dc98ac40148b429844eb44754673bc23c44600e
Asuma Yamada <asuma@linear-jp.biz>
2021-04-29 18:02:53 +0900

    fix: add domain to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   images: {
-    domains: ['lh3.googleusercontent.com', 'firebasestorage.googleapis.com'],
-  },
+    domains: [
+      'lh3.googleusercontent.com',
+      'firebasestorage.googleapis.com',
+      'next-firebase-chatapp-azure.vercel.app'
+    ]
+  }
 }
