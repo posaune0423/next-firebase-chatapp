d81c35bd0ab56d148d1e8c0ab4474db1443a8a59
Asuma Yamada <asuma@linear-jp.biz>
2021-04-29 23:42:34 +0900

    feat: import google font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,9 +7,14 @@ const App = ({ Component, pageProps }) => (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       <link rel="shortcut icon" href="/favicon.png" key="shortcutIcon" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Krona+One&family=Noto+Sans+JP:wght@300&display=swap"
+        rel="stylesheet"
+      />
       {/* <link rel="manifest" href="/manifest.json" /> */}
     </Head>
-      <Component {...pageProps} />
+    <Component {...pageProps} />
   </>
 )
 
