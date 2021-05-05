d6aef961c9bd453db5b89b3bf7709b2832ac6b3d
Asuma Yamada <asuma@linear-jp.biz>
2021-05-06 02:34:22 +0900

    fix: fix mypage

--- src/pages/mypage.tsx
+++ src/pages/mypage.tsx
@@ -6,6 +6,7 @@ import { Avatar, Button } from '@material-ui/core'
 import TextField from '@material-ui/core/TextField'
 import { FirebaseContext } from '../components/Firebase'
 import Header from '../components/Header'
+import mypageStyles from '../styles/components/mypage.module.css'
 
 export default function Home(): JSX.Element {
   const { currentUser } = useContext(AuthContext)
@@ -50,22 +51,22 @@ export default function Home(): JSX.Element {
 
   if (currentUser) {
     return (
-      <div style={{ marginTop: '56px' }}>
+      <div className={mypageStyles.root}>
         <Head>
           <title>Curabitur | My Page</title>
         </Head>
         <Header />
-        <div style={{ paddingTop: '2rem' }}>
+        <div className={mypageStyles.body}>
           <Avatar
             style={{ height: '70px', width: '70px', margin: '0 auto' }}
             src={
               userInfo ? userInfo.photoURL : 'https://api.adorable.io/avatars/23/abott@adorable.png'
             }
           />
-          <h2 id="simple-modal-title" style={{ textAlign: 'center' }}>
+          <h2 id="simple-modal-title" className={mypageStyles.h2}>
             {userInfo && userInfo.displayName}
           </h2>
-          <p id="simple-modal-description" style={{ fontSize: '12px', textAlign: 'center' }}>
+          <p id="simple-modal-description" className={mypageStyles.p}>
             {userInfo && userInfo.email}
           </p>
           <div style={{ padding: '20px' }}>
@@ -93,7 +94,7 @@ export default function Home(): JSX.Element {
     )
   } else {
     return (
-      <div style={{ textAlign: 'center', padding: '42vh 0' }}>
+      <div className={mypageStyles.progress}>
         <CircularProgress style={{ width: '60px', height: '60px' }} />
       </div>
     )
