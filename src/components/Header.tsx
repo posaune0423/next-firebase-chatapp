2876573843b27e20ce973f0d0a9282974439033e
Asuma Yamada <asuma@linear-jp.biz>
2021-05-06 00:49:06 +0900

    feat: add link to another pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,8 @@ import Menu from '@material-ui/core/Menu'
 import MenuItem from '@material-ui/core/MenuItem'
 import MenuIcon from '@material-ui/icons/Menu'
 import GitHubIcon from '@material-ui/icons/GitHub'
-import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle'
+import PolicyIcon from '@material-ui/icons/Policy'
 import { LogOut } from '../components/Buttons'
 import headerStyle from '../styles/components/header.module.css'
 
@@ -48,21 +49,31 @@ export default function Header(): JSX.Element {
           onClose={handleClose}
         >
           <MenuItem onClick={handleClose}>
-            <a
-              href="https://github.com/posaune0423/next-firebase-chatapp"
-              style={{ color: 'inherit' }}
-            >
-              <GitHubIcon />
+            <a href="/mypage/" style={{ color: 'inherit', textDecoration: 'none' }}>
+              <AccountCircleIcon style={{ verticalAlign: 'middle', marginRight: '1rem' }} />
+              mypage
             </a>
           </MenuItem>
           <MenuItem onClick={handleClose}>
-            <a href="https://www.linkedin.com/in/posaune0423/" style={{ color: 'inherit' }}>
-              <LinkedInIcon />
+            <a href="/policy/" style={{ color: 'inherit', textDecoration: 'none' }}>
+              <PolicyIcon style={{ verticalAlign: 'middle', marginRight: '1rem' }} />
+              privacy policy
+            </a>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <a
+              href="https://github.com/posaune0423/next-firebase-chatapp"
+              style={{ color: 'inherit', textDecoration: 'none' }}
+            >
+              <GitHubIcon style={{ verticalAlign: 'middle', marginRight: '1rem' }} />
+              source code
             </a>
           </MenuItem>
         </Menu>
         <Typography variant="h6" component="span" color="textPrimary" className={headerStyle.title}>
-          Curabitur
+          <a href="/" style={{ color: 'inherit', textDecoration: 'none' }}>
+            Curabitur
+          </a>
         </Typography>
         <LogOut />
       </Toolbar>
