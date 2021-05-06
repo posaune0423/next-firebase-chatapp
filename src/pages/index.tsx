a17c6702fc73b064c4b2416bea7997d9e9e9a416
Asuma Yamada <asuma@linear-jp.biz>
2021-05-07 01:37:18 +0900

    refactor: remove unnecessary imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,13 @@
-import Head from 'next/head'
 import { useContext } from 'react'
 import { AuthContext } from '../components/Auth'
-import { FirebaseProvider } from '../components/Firebase'
-import ChatRoom from '../components/ChatRoom'
-import Header from '../components/Header'
 import RoomList from '../components/RoomList'
 import Top from '../components/Top'
-import indexStyles from '../styles/components/index.module.css'
 
 export default function Home(): JSX.Element {
   const { currentUser } = useContext(AuthContext)
 
   if (currentUser) {
-    return (
-      <RoomList />
-    )
+    return <RoomList />
   } else {
     return <Top />
   }
